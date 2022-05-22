a0c338545efeacf1c5fa53e4416f031a1a683ebe
Manu Martinez <manuel.martinez@gammeranest.com>
2022-05-22 20:59:47 +0200

    First Commit

---
+++ DarkLifeExcalibur/Source/DarkLifeExcalibur/Private/Core/CPP_DarkLifeCharacter.cpp
@@ -0,0 +1,190 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/CPP_DarkLifeCharacter.h"
+
+// Sets default values
+ACPP_DarkLifeCharacter::ACPP_DarkLifeCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	
+
+	//Parameters Default Values
+	Health = 100;
+	MaxHealth = 100;
+	Stamina = 100;
+	MaxStamina = 100;
+	Fracture = 10;
+	Recharge = 10;
+	Defense = 10;
+
+	//Components Init
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetMesh());
+	SpringArm->SetRelativeLocation(FVector(-12.0f, 0.0f, 188.0f ));
+	SpringArm->SetRelativeRotation(FRotator(0.0f, 0.0f, 90.0f));
+	SpringArm->bUsePawnControlRotation = true;
+	SpringArm->TargetArmLength = 200.0f;
+
+	
+	LongBow = CreateDefaultSubobject<UChildActorComponent>(TEXT("Bow"));
+	LongBow->SetupAttachment(GetMesh(), "Bow_Back");
+
+	IgnisBomb = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Ignis"));
+	IgnisBomb->SetupAttachment(GetMesh(), "ignis");
+	IgnisBomb->SetVisibility(false, false);
+	
+	Excalibur = CreateDefaultSubobject<UChildActorComponent>(TEXT("Excalibur"));
+	Excalibur->SetupAttachment(GetMesh(), "Sword_Back");
+
+	ShieldMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Shield"));
+	ShieldMesh->SetupAttachment(GetMesh(), "Shield");
+
+	//Variables Default Values
+	EvasionSpeedValue = 1.5;
+	bBeastPowerMovement = false;
+	bSaveAttack = false;
+	bIsAttacking = false;
+	bDrawFinish = false;
+	bBlocking = false;
+	StaminaDividerMinLimit = -4.0;
+	StaminaSubstraction = 12.0;
+	StaminaIncreaseDelay = 1.0;
+	StaminaIncreaseTime = 0.01;
+	bSprintKeyPress = false;
+	bWalk = false;
+	WalkSpeed = 150.0;
+	RunSpeed = 350.0;
+	BeastPowerSpeed = 5000.0;
+	bSprint = false;
+	bLockedEnemy = false;
+	
+	
+}
+
+// Called when the game starts or when spawned
+void ACPP_DarkLifeCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACPP_DarkLifeCharacter::EvasionStepAnimations()
+{
+	if (Stamina >= 30.0) {
+		if (GetInputAxisValue("MoveRight") > 0) {
+			PlayAnimMontage(EvasionAnimations[0], EvasionSpeedValue);
+	  }
+		else {
+			if (GetInputAxisValue("MoveRight") < 0) {
+				PlayAnimMontage(EvasionAnimations[1], EvasionSpeedValue);
+			}
+			else {
+				if (GetInputAxisValue("MoveForward") > 0) {
+					PlayAnimMontage(EvasionAnimations[2], EvasionSpeedValue);
+				}
+				else if (GetInputAxisValue("MoveForward") < 0) {
+					SpringArm->bEnableCameraLag = false;
+					PlayAnimMontage(EvasionAnimations[3], EvasionSpeedValue);
+					SpringArm->bEnableCameraLag = true;
+				}
+			}
+		}
+	}
+
+	if (!bBeastPowerMovement) {
+
+		Stamina = UKismetMathLibrary::FClamp(Stamina - (StaminaSubstraction * 4.0), MaxStamina / StaminaDividerMinLimit, MaxStamina);
+	}
+
+}
+
+void ACPP_DarkLifeCharacter::StaminaIncrease()
+{
+	if (bBlocking) {
+		Stamina = UKismetMathLibrary::FClamp(Stamina + 0.15, 0.0, MaxStamina);
+	}
+	else {
+		Stamina = UKismetMathLibrary::FClamp(Stamina + 2.0, 0.0, MaxStamina);
+	}
+
+
+	if (Stamina == MaxStamina) {
+		GetWorldTimerManager().PauseTimer(StaminaIncreaseHandle);
+	}
+}
+
+void ACPP_DarkLifeCharacter::StaminaDecrease()
+{
+	Stamina = UKismetMathLibrary::FClamp(Stamina + (-0.3f),0.0f,MaxStamina);
+	if (Stamina <= 0.0f) {
+		StopSprint();
+	}
+}
+
+void ACPP_DarkLifeCharacter::PoisonIncrease()
+{
+	Health -= 0.2f;
+}
+
+void ACPP_DarkLifeCharacter::ResetCombo()
+{
+	bSaveAttack = false;
+	bIsAttacking = false;
+	GetWorldTimerManager().SetTimer(StaminaIncreaseHandle,this,&ACPP_DarkLifeCharacter::StaminaIncrease, StaminaIncreaseTime, true, StaminaIncreaseDelay);
+	
+}
+
+void ACPP_DarkLifeCharacter::StopSprint()
+{
+	bSprintKeyPress = false;
+	if (bBeastPowerMovement) {
+		GetCharacterMovement()->MaxWalkSpeed = BeastPowerSpeed;
+	}
+	else {
+		if (bWalk) {
+			GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+		}
+		else {
+			GetCharacterMovement()->MaxWalkSpeed = RunSpeed;
+		}
+	}
+
+	bSprint = false;
+
+	if (StaminaIncreaseHandle.IsValid()) {
+		GetWorldTimerManager().PauseTimer(StaminaDecreaseHandle);
+		GetWorldTimerManager().ClearTimer(StaminaDecreaseHandle);
+	}
+
+	if (bLockedEnemy) {
+		SpringArm->CameraLagSpeed = 30.0f;
+	}
+
+	GetWorldTimerManager().SetTimer(StaminaIncreaseHandle, this, &ACPP_DarkLifeCharacter::StaminaIncrease, StaminaIncreaseTime, true, 0.0f);
+
+}
+
+void ACPP_DarkLifeCharacter::ShootArrow()
+{
+	bDrawFinish = false;
+	//PlayAnimMontage(DrawBowAnimation);
+	PlayAnimMontage(ReadyBowAnimation,1.0f,"Aim");
+}
+
+// Called every frame
+void ACPP_DarkLifeCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACPP_DarkLifeCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ DarkLifeExcalibur/Source/DarkLifeExcalibur/Public/Core/CPP_DarkLifeCharacter.h
@@ -0,0 +1,136 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Components/ChildActorComponent.h"
+#include "CPP_DarkLifeCharacter.generated.h"
+
+UCLASS()
+class DARKLIFEEXCALIBUR_API ACPP_DarkLifeCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACPP_DarkLifeCharacter();
+
+	//Character Parameters
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+		double Health;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+		double MaxHealth;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+		double Stamina;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+		double MaxStamina;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+		double Fracture;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+		double Recharge;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+		double Defense;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+		double MaxBeast;
+
+	//Character Components
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameters")
+		USpringArmComponent* SpringArm;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameters")
+		UChildActorComponent* Excalibur;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameters")
+		UChildActorComponent* LongBow;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameters")
+		UStaticMeshComponent* ShieldMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameters")
+		USkeletalMeshComponent* IgnisBomb;
+
+	//Character Animations
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animations")
+		TArray<UAnimMontage*> EvasionAnimations;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animations")
+		UAnimMontage* DrawBowAnimation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animations")
+		UAnimMontage* ReadyBowAnimation;
+
+	//Character Variables
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Special Movements")
+		bool bBeastPowerMovement;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		double EvasionSpeedValue;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		bool bSprintKeyPress;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		bool bWalk;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		bool bSprint;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		double WalkSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		double RunSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		double BeastPowerSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameters|Modifiers")
+		double StaminaDividerMinLimit;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameters|Modifiers")
+		double StaminaSubstraction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameters|Modifiers")
+		double StaminaIncreaseDelay;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameters|Modifiers")
+		double StaminaIncreaseTime;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+		bool bSaveAttack;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+		bool bIsAttacking;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+		bool bBlocking;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+		bool bDrawFinish;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+		bool bLockedEnemy;
+
+	
+	
+		
+protected:
+
+	//Timers Handlers
+	UPROPERTY()
+		FTimerHandle StaminaIncreaseHandle;
+	UPROPERTY()
+		FTimerHandle StaminaDecreaseHandle;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+
+	UFUNCTION(BlueprintCallable, Category = "Animations")
+		void EvasionStepAnimations();
+	UFUNCTION(BlueprintCallable, Category = "Parameters|Modifiers")
+		void StaminaIncrease();
+	UFUNCTION(BlueprintCallable, Category = "Parameters|Modifiers")
+		void StaminaDecrease();
+	UFUNCTION(BlueprintCallable, Category = "Parameters|Modifiers")
+		void PoisonIncrease();
+	UFUNCTION(BlueprintCallable, Category = "Attack")
+		void ResetCombo();
+	UFUNCTION(BlueprintCallable, Category = "Attack")
+		void StopSprint();
+	UFUNCTION(BlueprintCallable, Category = "Attack")
+		void ShootArrow();
+
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};

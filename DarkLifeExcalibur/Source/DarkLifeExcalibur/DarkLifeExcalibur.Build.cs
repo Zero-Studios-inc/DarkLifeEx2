104f82422d1fd0a61c717581fc5fbec09d2889e7
cesarzero1992 <67668367+cesarzero1992@users.noreply.github.com>
2022-10-06 04:35:28 -0400

    cambioa para build

--- DarkLifeExcalibur/Source/DarkLifeExcalibur/DarkLifeExcalibur.Build.cs
+++ DarkLifeExcalibur/Source/DarkLifeExcalibur/DarkLifeExcalibur.Build.cs
@@ -8,7 +8,7 @@ public class DarkLifeExcalibur : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "AIModule", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 

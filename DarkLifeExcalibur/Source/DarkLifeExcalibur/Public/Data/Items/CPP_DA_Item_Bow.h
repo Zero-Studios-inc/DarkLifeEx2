6c9ea66f1f8e8b1f17dc29f94edfd0cff5475f4b
raynerbest <65691143+raynerbest@users.noreply.github.com>
2024-07-19 21:26:15 -0400

    Adding Bow as an Item

---
+++ DarkLifeExcalibur/Source/DarkLifeExcalibur/Public/Data/Items/CPP_DA_Item_Bow.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/Items/CPP_DA_Item.h"
+#include "CPP_DA_Item_Bow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DARKLIFEEXCALIBUR_API UCPP_DA_Item_Bow : public UCPP_DA_Item
+{
+	GENERATED_BODY()
+	
+};
